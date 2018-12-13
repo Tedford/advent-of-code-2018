7fa6bf544fc676b3bf0f50f006de76744a0845b5
Tedford <dev@tedfordjohnson.com>
2018-12-13 11:23:48 -0500

    Day 6 - Part 2

--- day6/day6.fsx
+++ day6/day6.fsx
@@ -4,8 +4,11 @@ open System.IO
 type Assignment =
     | Unassigned
     | Common of Distance : int
+    | Anchor of Id : string
     | Coordinate of Id : string * Distance : int
-
+    | Infinite
+    | Finite of Distance : int
+    
 type Distance = 
     | Closer
     | Equal
@@ -35,8 +38,16 @@ let assignId id =
 
 let parseRow id row =
     match row with
-    | Match @"(\d+),\s(\d+)" [_; x; y] -> { Id = assignId id; X = x.Value |> int; Y = y.Value |> int} |> Some
+    // transpose x & y so that the 2D array coordinates match the problem sample
+    | Match @"(\d+),\s(\d+)" [_; y;x] -> { Id = assignId id; X = x.Value |> int; Y = y.Value |> int} |> Some
     | _->None
+    
+let printMap (xn,yn) (map:Assignment[,]) =
+    printfn "\nMap"
+    for x in 0 .. xn do
+        for y in 0 .. yn do
+            match map.[y,x] with Coordinate(id,_) -> printf "%2s " (id.ToLower()) | Anchor id -> printf "%2s " id | Infinite -> printf " 8 " | Finite _ -> printf " # " | _ -> printf " . "
+        printf "\n"
 
 let locations =
     File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day6\input.dat")
@@ -59,7 +70,7 @@ let Yn = locations |> Seq.map (fun r -> r.Y) |> Seq.max
 
 printfn "Bounding Box\n\t%dx%d to %dx%d\n" Xi Yi Xn Yn
 
-let plane = Array2D.create (Xn + 1) (Yn + 1) Unassigned
+let plane = Array2D.create (Yn + 1) (Xn + 1) Unassigned
 
 printfn "Assigning Zones"
 locations 
@@ -67,44 +78,41 @@ locations
     for x in Xi .. Xn do
         for y in Yi .. Yn do
             let distance' = manhattanDistance (x,y) (loc.X, loc.Y)
-            let id = 
-                if distance' = 0 then 
-                    printfn "\tzone %s at %dx%d" loc.Id loc.X loc.Y
-                    loc.Id.ToUpper()
-                else
-                    loc.Id.ToLower()
-            match plane.[x,y] with
-            | Unassigned -> plane.[x,y] <- Coordinate(id, distance')
-            | Common distance -> 
+            if distance' = 0 then printfn "\tzone %s at %dx%d" loc.Id loc.X loc.Y
+            
+            match distance', plane.[y,x] with
+            | 0, _ -> plane.[y,x] <- Anchor(loc.Id) 
+            | _,Unassigned -> plane.[y,x] <- Coordinate(loc.Id, distance')
+            | _,Common distance -> 
                 match relativeDistance distance distance' with
-                | Closer -> plane.[x,y] <- Coordinate(id, distance')
+                | Closer -> plane.[y,x] <- Coordinate(loc.Id, distance')
                 | _ -> ()
-            | Coordinate (_,distance) ->
+            | _,Coordinate (_,distance) ->
                 match relativeDistance distance distance' with
-                | Closer -> plane.[x,y] <- Coordinate(id, distance')
-                | Equal -> plane.[x,y] <- Common(distance)
+                | Closer -> plane.[y,x] <- Coordinate(loc.Id, distance')
+                | Equal -> plane.[y,x] <- Common(distance)
                 | _ -> ()
+            | _ -> ()
 )
 
 let infiniteZones = 
     seq {
         for x in Xi .. Xn do
-            match plane.[x,Yi] with
-            | Coordinate (id,_) -> yield id.ToUpper() |> Some
+            match plane.[Yi,x] with
+            | Coordinate (id,_) -> yield id |> Some
             | _ -> yield None
             
-            match plane.[x,Yn] with
-            | Coordinate (id,_) -> yield id.ToUpper() |> Some
+            match plane.[Yn,x] with
+            | Coordinate (id,_) -> yield id |> Some
             | _ -> yield None
-     
             
         for y in Yi .. Yn do
-            match plane.[Xi,y] with
-            | Coordinate (id,_) -> yield id.ToUpper() |> Some
+            match plane.[y,Xi] with
+            | Coordinate (id,_) -> yield id |> Some
             | _ -> yield None
             
-            match plane.[Xn,y] with
-            | Coordinate (id,_) -> yield id.ToUpper() |> Some
+            match plane.[y,Xn] with
+            | Coordinate (id,_) -> yield id |> Some
             | _ -> yield None
     }
     |> Seq.choose id
@@ -115,25 +123,75 @@ printfn "\nInfinite Zones:"
 infiniteZones
 |> Seq.iter (fun z -> printfn "\t%s" z)
 
-let isInfineZone (id:string) = infiniteZones |> Seq.contains id |> not
+let threshold = 10000
+printfn "\nAllowed Threshold %d" threshold
+
+let isInfineZone (id:string) = infiniteZones |> Seq.contains id
 
 let zones =
     seq {
         for x in Xi .. Xn do
             for y in Yi .. Yn ->
-                match plane.[x,y] with
-                | Coordinate (id,_) -> id.ToUpper() |> Some
+                match plane.[y,x] with
+                | Coordinate (id,_) -> id |> Some
                 | _ -> None}
     |> Seq.choose id
     |> Seq.groupBy id
-    |> Seq.map (fun (id,occurrances) -> {Id = id; Area = Seq.length occurrances; Infinte = isInfineZone id |> not})
+    |> Seq.map (fun (id,occurrances) -> {Id = id; Area = (+) (Seq.length occurrances) 1; Infinte = isInfineZone id})
     |> Seq.sortByDescending (fun z -> z.Area)
 
+
+let finiteZones = zones |> Seq.filter (fun z -> z.Infinte |> not) |> Seq.toArray
+
 printfn "\nFinite Zones:"
-zones |> Seq.filter (fun z -> z.Infinte |> not) |> Seq.iter (fun z-> printfn "\tID: %2s Area: %4d" z.Id z.Area)
+finiteZones |> Seq.iter (fun z-> printfn "\tID: %2s Area: %4d" z.Id z.Area)
+
+printMap (Xn,Yn) plane
+
+let globalCost (x,y) =
+    locations
+    |> Seq.map (fun l -> manhattanDistance (x,y) (l.X,l.Y))
+    |> Seq.sum
+
+for x in Xi .. Xn do
+    for y in Yi .. Yn do
+        match plane.[y,x] with
+        | Coordinate (_,_) -> plane.[y,x] <- Finite(globalCost (x,y))
+        | Common _ -> plane.[y,x] <- Finite (globalCost(x,y))
+        | _ -> ()
 
 
-//for x in Xi .. Xn do
-//    for y in Yi .. Yn do
-//        match plane.[x,y] with Coordinate(id,_) -> printf "%2s " id | Common _ -> printf " . " | _ -> ()
-//    printf "\n"
+let calculateSafeArea threshold =
+    seq {
+    for x in Xi .. Xn do
+        for y in Yi .. Yn ->
+            match plane.[y,x] with
+            | Finite distance when distance < threshold -> 1
+            | _ -> 0
+    }
+    |> Seq.sum
+
+
+// attempt #1 - 37282 too low
+// Attempt #2 - Too low
+//    Safe Area 38094
+//    Safe Anchors 28
+//    Total 38122
+// Attempt #3 - 
+//    Safe Area 46514
+//    Safe Anchors 28
+//    Total 46542
+
+let safeArea = (calculateSafeArea threshold) 
+let safeAnchors = locations |> Seq.map (fun loc-> globalCost (loc.X, loc.Y)) |> Seq.filter (fun cost -> cost < threshold) |> Seq.length 
+printfn  "\nSafe Area %d\nSafe Anchors %d\nTotal %d" safeArea safeAnchors (safeArea + safeAnchors)
+
+let printMapWithCost (xn,yn) threshold (map:Assignment[,]) =
+    printfn "\nMap"
+    for x in 0 .. xn do
+        for y in 0 .. yn do
+            match map.[y,x] with Coordinate(id,_) -> printf "%2s " (id.ToLower()) | Anchor id -> printf "%2s " id | Infinite -> printf " 8 " | Finite x when x < threshold -> printf " # " | _ -> printf " . "
+        printf "\n"
+
+
+printMapWithCost (Xn,Yn) threshold plane
