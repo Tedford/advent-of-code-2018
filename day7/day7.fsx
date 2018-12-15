21f6c6db3566d1cf57eead2327f21ebfe5958674
Tedford <dev@tedfordjohnson.com>
2018-12-15 02:09:38 -0500

    Day 7 - Part 1

--- day7/day7.fsx
+++ day7/day7.fsx
@@ -1,97 +1,64 @@
 open System
 open System.IO
 
-type Given  = {
-    Id: string;
-    Requires: string seq;
-    Cost: int
-}
-
 let (|Match|_|) pattern input =
     let m = Regex.Match(input, pattern, RegexOptions.Compiled)
     match m.Success with
     | true -> Some[for i in m.Groups->i]
     | _ -> None
 
+let merge a b =
+    Map.fold (fun s k v ->
+        match Map.tryFind k s with
+        | Some _ -> s
+        | None -> Map.add k v s) a b
+
 let parseRow row =
     match row with
-    | Match @"Step (\w+) must be finished before step (\w+) can begin." [_;predecessor; successor] -> (predecessor.Value,successor.Value)
+    | Match @"Step (\w+) must be finished before step (\w+) can begin." [_;predecessor; successor] -> (successor.Value,predecessor.Value)
     | _ -> failwith "Unexpected input format"
 
-//let givens = [File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day7\example.dat") |> Seq.map parseRow |> Seq.groupBy fst |> Seq.map (fun (p,s)->{Id = p; Requires = s |> Seq.map (fun (k,v) -> {Id = v; Requires = []}) |> Seq.toList } )]
-//let givens = [File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day7\example.dat") |> Seq.map parseRow |> Seq.groupBy fst |> Seq.map (fun (p,s)->{Id = p; Requires = s |> Seq.map snd |> Seq.toList} )] 
-//let givens = File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day7\input.dat") |> Seq.map parseRow |> Seq.groupBy fst |> Seq.map (fun (k,v) -> (k, v|> Seq.map snd))|> Map.ofSeq
-let raw = File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day7\example.dat") |> Array.map parseRow 
+// example - CABDFE
+// example 2 - AFGYZ
+let raw = File.ReadAllLines(@"c:\projects\github\advent-of-code-2018\day7\input.dat") |> Array.map parseRow 
 
-let costs =
-    raw
-    |> Seq.map snd
-    |> Seq.groupBy id
-    |> Seq.map (fun (k,l)-> (k, Seq.length l))
-    |> Map.ofSeq
+// all of the system states
+let states = raw |> Array.toList |> List.collect (fun (s,p) -> [yield p; yield s]) |> List.distinct |> List.sort
 
-let getCost state = match costs.TryFind state with | Some cost -> cost | _ -> 0
+// a record of which states have been built
+let built = states |> Seq.map (fun id -> (id,false)) |> Map.ofSeq
 
-let dependencies = raw  |> Seq.groupBy fst |> Seq.map (fun (k,v) -> (k, v |> Seq.map snd )) |> Map.ofSeq
+// a mapping of each state and its prerequsites
+let dependencies = 
+    merge 
+        (raw |> Seq.groupBy fst |> Seq.map (fun (k,v)-> (k,  v |> Seq.map snd |> Seq.toArray )) |> Map.ofSeq)
+        (states |> List.map (fun s -> (s,Array.empty<string>))|> Map.ofList)
 
-let getDependencies state = match dependencies.TryFind state with | Some x -> x |> Seq.sort | _ -> Seq.empty
+printfn "Dependencies Chain"
+dependencies |> Map.iter (fun k -> printfn "\t%s := %A" k)
 
-let states = 
-    raw
-    |> Seq.collect (fun (p,s) -> seq {yield p; yield s})
-    |> Seq.distinct
-//    |> Seq.map (fun id -> {Id = id; Cost = getCost id; Requires = getDependencies id})
-    |> Seq.map (fun id -> {Id = id; Cost = getCost id; Requires = getDependencies id})
-    |> Seq.sortBy (fun s -> s.Cost, s.Id)
-    |> Seq.toArray
-
-printfn "\nStates:"
-states|> Seq.iter (fun s -> printfn "\tID: %s Cost: %d Requirements: %d %A" s.Id s.Cost (Seq.length s.Requires) s.Requires)
-
-let order = 
-    states 
-    |> Seq.groupBy (fun g -> g.Cost)
-    |> Seq.collect (fun (_,v) -> v |> Seq.map (fun g -> g.Id) |> Seq.sort)
-    |> String.Concat
-
-// ACEUBDLSMNXZIKWYQTFGPVHJRO incorrect
-printfn "\nOrder: %s" order
+let rec nextState states (built: Map<string,bool>) =
+    match states with
+    | x :: xs -> 
+        match built.[x] with
+        | true -> nextState xs built
+        | false ->
+            let d= dependencies.[x]
+            match Seq.length d with
+            | 0 -> x
+            | _ -> match d |> Seq.map (fun p -> if built.[p] then 1 else 0) |> Seq.sum = Seq.length d with true -> x | false -> nextState xs built
+    | _ -> failwith "All states have been built"
 
 
-(*
-printfn "\nGivens:"
-givens.Dump()
-
-let states = 
-    givens
-    |> Map.toArray
-    |> Seq.collect (fun (k, v) -> [k] @ (v|> Seq.toList))
-    |> Seq.distinct
-    |> Seq.sort
-    
-
-printfn "\nDetected States:"
-states |> Seq.iter (fun s -> printfn "\t%s" s)
-
-let cost = 
-    states
-    |> Seq.map(fun s -> givens.TryFind s)
-    |> Seq.choose id
-    |> Seq.collect id
-    |> Seq.groupBy id
-    |> Seq.map(fun (k,l)-> (k,Seq.length l))
-    |> Map.ofSeq
-
-
-cost.Dump()
-*)
-
-
-//givens
-//|> Seq.fold ( fun graph given ->
-//    match graph with 
-//    | [] -> {Id = given.Predecessor; Requires = [{Id = given.Successor; Requires = []}]}
-//    | x::xs ->
-//        match x.Id with
-//        | given.Predecessor -> 
-//        ) []
+Seq.unfold (fun (s,h) -> 
+    match s with 
+    | [] -> None
+    | _ ->
+        let next = nextState s h
+        let history' = Map.add next true h
+        let toBuild' = s |> List.filter (fun p -> p <> next)
+        Some((next, Map.empty<string,bool>), (toBuild',history'))
+) (states, built)
+|> Seq.map fst
+|> String.Concat
+|> printfn "\nWorkflow: %s" 
